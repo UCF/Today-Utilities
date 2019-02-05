cd7f6248b82007409b30f8e09d77afab3f9d9bac
Jo Dickson <jogreybill@gmail.com>
2019-02-05 12:02:56 -0500

    Initial template file cleanup

---
+++ today-utilities.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Today Utilities
+Description: Feature and utility plugin for the UCF Today website
+Version: 0.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/Today-Utilities
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
