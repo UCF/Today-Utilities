7953ebf0989b9267e8b0406cb3c5774a20733c6d
Jo Dickson <jogreybill@gmail.com>
2019-05-31 11:59:38 -0400

    Added missing support for category_slugs and tag_slugs params in REST API results for posts

--- includes/today-feeds.php
+++ includes/today-feeds.php
@@ -83,3 +83,44 @@ function tu_add_author_byline_to_post_feed() {
 }
 
 add_action( 'rest_api_init', 'tu_add_author_byline_to_post_feed' );
+
+
+/**
+ * Adds support for "category_slugs" and "tag_slugs" params
+ * in REST API results for posts.
+ *
+ * Ported over from Today-Bootstrap
+ *
+ * @since 1.0.3
+ */
+function tu_add_tax_query_to_posts_endpoint( $args, $request ) {
+	$params = $request->get_params();
+
+	$tax_query = array();
+
+	if ( isset( $params['category_slugs'] ) ) {
+		$tax_query[] =
+			array(
+				'taxonomy' => 'category',
+				'field'    => 'slug',
+				'terms'    => $params['category_slugs']
+			);
+	}
+
+	if ( isset( $params['tag_slugs'] ) ) {
+		$tax_query[] =
+			array(
+				'taxonomy' => 'post_tag',
+				'field'    => 'slug',
+				'terms'    => $params['tag_slugs']
+			);
+	}
+
+	if ( count( $tax_query ) > 0 ) {
+		$args['tax_query'] = $tax_query;
+	}
+
+	return $args;
+}
+
+add_action( 'rest_post_query', 'tu_add_tax_query_to_posts_endpoint', 2, 10 );
