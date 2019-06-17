acf1a4a364abe434eba559e57516ece186e29abc
Jo Dickson <jogreybill@gmail.com>
2019-06-17 11:13:09 -0400

    Updated version number and changelog

--- today-utilities.php
+++ today-utilities.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Today Utilities
 Description: Feature and utility plugin for the UCF Today website
-Version: 1.0.5
+Version: 1.0.6
 Author: UCF Web Communications
 License: GPL3
 GitHub Plugin URI: UCF/Today-Utilities
